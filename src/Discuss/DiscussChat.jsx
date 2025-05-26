cb4051a4ae68d6400d659b9b7a0f9b081f88ba5f
Victor <vbivol394@gmail.com>
2025-05-27 02:05:54 +0300

    am facut logica partial la conponenta Discuss

--- src/Discuss/DiscussChat.jsx
+++ src/Discuss/DiscussChat.jsx
@@ -1,14 +1,47 @@
 import "./DiscussChat.css";
+import { useEffect, useState } from 'react';
 
 function DiscussChat(){
+    const [messages, setMessages] = useState(() => {
+        const savedMessages = localStorage.getItem("discussChat");
+        return savedMessages ? JSON.parse(savedMessages) : [];
+    });
+    const [chatInput, setChatInput] = useState("");
+
+    const handleInputChange = (e) => {
+        setChatInput(e.target.value);
+    }
+
+    const sendInput = () => {
+        if (chatInput.trim() !== "") {
+            let newMessages = [...messages, chatInput];
+            if(newMessages.length > 8) {
+                newMessages = newMessages.slice(newMessages.length - 8);
+            }
+            setMessages(newMessages);
+            setChatInput("");
+        }
+    }
+
+    useEffect(() => {
+        localStorage.setItem("discussChat", JSON.stringify(messages));
+    }, [messages]);
+
     return (
         <div className="blockChat">
             <div className="chat">
-                asdad
+                {messages.map((message, index) => (
+                    <div className="message" key={index}>
+                        {message}
+                    </div>
+                ))}
             </div>
-            <input className="chatbar" type="text" />
-
+            <div className="inputcont">
+                <input className="chatbar" value={chatInput} onChange={handleInputChange} type="text" />
+                <button className="sendButton" onClick={sendInput}>Send</button>
+            </div>
         </div>
     );
 }
+
 export default DiscussChat;
